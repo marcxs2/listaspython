b7cb3c7ee4b426c84acd9d33950d277778ad99d7
marcxs2 <bichitoraptor22@gmail.com>
2022-02-05 11:51:09 +0100

    Primera Versión

---
+++ listaspython.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Feb  5 11:31:23 2022
+
+@author: marcos
+"""
+
+# Importo la librería random para generar números aleatorios.
+
+import random as rm
+
+# Función que crea una lista con 20 números aleatorios del 1 al 200.
+
+def crearlista():
+    global lista
+    lista=[]
+    for _ in range(20):
+        lista.append(rm.randint(1,200))
+       
+# Llamo a la función que genera la lista.
+
+crearlista()
